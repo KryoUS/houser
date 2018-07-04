d74ff8a6528ec7196a397aeff833b9ec715324fd
unknown <TaurusSHO89@gmail.com>
2018-07-03 19:39:18 -0600

    Setup > Database, complete

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const controller = require(__dirname + '/controller')
+const massive = require('massive')
+require('dotenv').config()
+
 // const cors = require('cors')
 const app = express()
 
 app.use( bodyParser.json())
 // app.use(cors())
+
+massive( process.env.CONNECTION_STRING ).then( dbInstance => {
+    app.set('db', dbInstance)
+}).catch( error => console.log(`Heroku Database Connection Error`, error) );
 
 const apiBaseUrl = "/api/stuff"
 
